6beac1aefc6be7f00d8b3aa773d0f350917bf4db
Mukul Kumar <mukulkumarsharma540@gmail.com>
2025-04-19 18:09:29 +0530

    completed ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,68 @@
 import './App.css'
 import Navbar from './components/navbar'
-import todo from './assets/todo.png'
-import done from './assets/done.png'
+import todo1 from './assets/todo.png'
+import done1 from './assets/done.png'
 import Card from './components/taskCard'
 function App() {
   const tasks = [
     {
       title: "Finish React project",
       category: "Work",
-      priority: "High"
+      priority: "High",
+      done:false,
     },
     {
       title: "Grocery shopping",
       category: "Personal",
-      priority: "Medium" //#FB8C00
+      priority: "Medium", //#FB8C00
+      done:false,
     },
     {
       title: "Read a book",
       category: "Leisure",
-      priority: "Low" //#66BB6A
+      priority: "Low" ,//#66BB6A
+      done:false,
+    },
+    {
+      title: "Finish React project",
+      category: "Work",
+      priority: "High",
+      done: false
+    },
+    {
+      title: "Grocery shopping",
+      category: "Personal",
+      priority: "Medium",
+      done: true,
+    },
+    {
+      title: "Read a book",
+      category: "Leisure",
+      priority: "Low",
+      done: true,
     }
   ];
+  const todo = tasks.filter((ele)=>{
+    return ele.done === false
+  })
+  const done = tasks.filter((ele)=>{
+    return ele.done === true
+  })
   return(
     <div className='main'>
       <Navbar></Navbar>
       <div className="body">
         <div className="todo align">
-          <div><img src={todo} className='im'/><h1 className='heading'>To-do</h1></div>
-          {tasks.map((ele)=>{
+          <div><img src={todo1} className='im'/><h1 className='heading'>To-do</h1></div>
+          {!(todo.length === 0) ? todo.map((ele)=>{
             return <Card title = {ele.title} category = {ele.category} priority={ele.priority}/>
-          })}
+          }): <h3 className='status'>Add some tasks</h3>}
         </div>
-        <div className="done align">
-          <div><img src={done} className='im'/><h1 className='heading'>Done</h1></div>
+        <div className="done1 align">
+          <div><img src={done1} className='im'/><h1 className='heading'>Done</h1></div>
+          { !(done.length === 0) ? done.map((ele)=>{
+            return <Card title = {ele.title} category = {ele.category} priority={ele.priority} doneCard = "done"/>
+          }) : <h3 className='status' >No tasks completed</h3>}
         </div>
       </div>
     </div>
--- src/components/taskCard.jsx
+++ src/components/taskCard.jsx
@@ -1,16 +1,39 @@
+import { useState, useEffect } from 'react';
 import '../App.css'
+import edit from '../assets/edit.png'
+import trash from '../assets/trash.png'
+import Button from './button';
+import Input from './inputbar';
 function Card(props) {
+    const [color, setColor] = useState('')
+    function Color() {
+        if (props.priority === "High") {
+            setColor("high")
+        }
+        else if (props.priority === "Medium") {
+            setColor("medium")
+        }
+        else if (props.priority === "Low") {
+            setColor("low")
+        }
+    }
+    useEffect(Color, [color])
     return (
-        <div className="card">
-            <div className="left">
-                <div className="title">{props.title}</div>
-                <div className="category">{props.category}</div>
+        <div className={`cardParent ${props.doneCard}`}>
+            <div className="checkbox">
+                <Input type="checkbox" />
             </div>
-            <div className="right">
-                <div className="pri">{props.priority}</div>
-                <div className="crud">
-                    <div className="edit">Edit</div>
-                    <div className="delete">Delete</div>
+            <div className={`card ${props.doneCard}`}>
+                <div className="left">
+                    <div className="title">{props.title}</div>
+                    <div className="category">{props.category}</div>
+                </div>
+                <div className="right">
+                    <div className={`pri ${color}`}>{props.priority}</div>
+                    <div className="crud">
+                        <div className="edit"><Button name={<img src={edit} alt="" />} class="editBtn" /></div>
+                        <div className="delete"><Button name={<img src={trash} alt="" />} class="editBtn" /></div>
+                    </div>
                 </div>
             </div>
         </div>
